cdba87e2e4f5f0c62b9437ca6641d90a393af9e4
kunjjarsaniya <kunjjarsaniya07@gmail.com>
2025-10-02 13:35:14 +0530

    fix email-rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
